28dc694f435c41876ddfd6b20c57c0bfad257ebb
Athang Gupte <athang213@gmail.com>
2023-09-29 15:34:22 -0700

    Customizations and basic content added

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Athang Gupte | Personal Portfolio Website';
+export const SITE_DESCRIPTION = 'This site is the personal portfolio of Athang Gupte, a game engine developer and software engineer with a passion for creating unique and innovative interactive experiences.';
